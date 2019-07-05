239019551b326cce3832ff15bb32ca93edb54613
John Sumskas <john.sumskas@gmail.com>
2019-07-05 12:12:22 +1000

    added autonomy package

---
+++ kingfisher_remote_control/scripts/remote_controler.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from __future__ import print_function
+# so i can use it with my list comprehensions
+import rospy
+from std_msgs.msg import Float32
+import sys
+import time
+import serial
