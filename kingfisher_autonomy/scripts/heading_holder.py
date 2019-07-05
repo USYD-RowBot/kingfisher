e3ff21f13c9bc035a8f978944b448034b01e51a1
John Sumskas <john.sumskas@gmail.com>
2019-07-05 14:31:32 +1000

    added yaw rate

--- kingfisher_autonomy/scripts/heading_holder.py
+++ kingfisher_autonomy/scripts/heading_holder.py
@@ -12,12 +12,13 @@ class Node():
         self.left = 0
         self.right = 0
         self.desired_heading = 0 #Radianns
-        self.prev_heading = 0
+        self.yaw_rate = 0
 
     def imu_callback(self,imu_msg):
         (roll, pitch, yaw) = euler_from_quaternion ([imu_msg.orientation.x,imu_msg.orientation.y,imu_msg.orientation.z,imu_msg.orientation.w])
         self.current_heading = yaw
         print("YAW:", math.degrees(yaw))
+        self.yaw_rate = imu_msg.angular_velocity.z
 
     def heading_callback(self,data):
         self.desired_heading = math.radians(data.data)
@@ -32,11 +33,13 @@ if __name__=="__main__":
     pub_right = rospy.Publisher("right", Float32, queue_size = 10)
     rate = rospy.Rate(20)
     while not rospy.is_shutdown():
-        K = 500
+        Kp = 500
+        Kd = 50
         err = node.current_heading - node.desired_heading
         #dt = node.current_heading - previous_heading
-        node.left = err*K
-        node.right = -err*K
+        node.left = err*K + node.yaw_rate * Kd
+        node.right = -err*K - node.yaw_rate*Kd
+
 
         if (node.left>100):
             node.left = 100
