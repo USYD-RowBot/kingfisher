5ffef389e6e3dcfa629418f08ce25505b3c3f61d
spar3321 <spar3321@uni.sydney.edu.au>
2019-07-18 13:46:44 +1000

    added kalman filter code

--- kingfisher_localisation/src/kalman_gps_imu.py
+++ kingfisher_localisation/src/kalman_gps_imu.py
@@ -3,6 +3,7 @@ import rospy
 from sensor_msgs.msg import Imu
 from sensor_msgs.msg import NavSatFix
 from math import *
+import numpy as np
 
 class Node():
     def __init__(self):
@@ -14,7 +15,55 @@ class Node():
         self.orientation = None
         self.odom_pub = None
 
+    def kalman_predict(self, dt, x, y, yaw, lin_vel, ang_vel,state_noise, vel_noise, mod_noise):
+        # transition matrix
+        fx = np.array([1, 0, -dt*lin_vel*np.sin(yaw)],
+                      [0, 1,  dt*lin_vel*np.cos(yaw)],
+                      [0, 0, 1])
+        # input matrix
+        fu = np.array([dt * np.cos(yaw), 0],
+                      [dt * np.sin(yaw), 0],
+                      [0, 1])
 
+        # perform a prediction
+        state_est = np.array([x + dt*lin_vel*np.cos(yaw)],
+                             [y + dt*lin_vel*np.sin(yaw)],
+                             [yaw + dt*ang_vel])
+        state_noise = fx*state_noise*fx.transpose() + fu*vel_noise*fu.transpose()+ mod_noise
+
+        # HAVE TO RETURN STATE_EST AND STATE_NOISE TO MAIN PROGRAM
+
+    def kalman_update_gps(self, x, y, yaw, gps_x, gps_y, h_gps,state_noise, gps_noise):
+        # sensor reading
+        z = np.array([[gps_x],[gps_y]])
+
+        # perform an update
+        z_est = h_gps*np.array([[x],[y],[yaw]])
+        innovation = z-z_est
+        s = h_gps*state_noise*h_gps.transpose() + gps_noise
+        w = state_noise*h_gps.transpose()*np.linalg.inv(s)
+
+        state_est = np.array([[x],[y],[yaw]]) + w*innovation
+        state_noise = state_noise - w*s*w.transpose()
+
+        # HAVE TO RETURN STATE_EST AND STATE_NOISE TO MAIN PROGRAM
+
+    def kalman_update_imu(self, x, y, yaw, imu_yaw, h_imu,state_noise, imu_noise):
+        # sensor reading
+        z = np.array([imu_yaw])
+
+        # NEED TO MAKE SURE THAT THE ANGLE READINGS ARE CORRECT I.E CIRCULAR
+
+        # perform an update
+        z_est = h_imu * np.array([[x], [y], [yaw]])
+        innovation = z - z_est
+        s = h_imu * state_noise * h_imu.transpose() + imu_noise
+        w = state_noise * h_imu.transpose() * np.linalg.inv(s)
+
+        state_est = np.array([[x], [y], [yaw]]) + w * innovation
+        state_noise = state_noise - w * s * w.transpose()
+
+        # HAVE TO RETURN STATE_EST AND STATE_NOISE TO MAIN PROGRAM
 
     def gps_callback(self,data):
         self.lat = data.latitude
